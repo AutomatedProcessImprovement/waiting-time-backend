dc606aa62b4ac71962f6d245dcc388ba04d02d99
Ihar Suvorau <ihar.suvorau@gmail.com>
2022-07-13 18:00:09 +0300

    Building for more platforms

--- build.bash
+++ build.bash
@@ -3,7 +3,7 @@
 package="github.com/AutomatedProcessImprovement/waiting-time-backend"
 package_name="waiting-time-backend"
 
-platforms=("linux/amd64")
+platforms=("linux/amd64" "darwin/amd64" "darwin/arm64" "windows/amd64")
 
 for platform in "${platforms[@]}"
 do
