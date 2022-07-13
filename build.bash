816f1d7a842b37d6924df9c7d99023eba55ad5c5
Ihar Suvorau <ihar.suvorau@gmail.com>
2022-07-13 17:57:29 +0300

    Build script updated to include assets

--- build.bash
+++ build.bash
@@ -3,21 +3,40 @@
 package="github.com/AutomatedProcessImprovement/waiting-time-backend"
 package_name="waiting-time-backend"
 
-platforms=("darwin/amd64" "darwin/arm64" "linux/amd64" "linux/arm64" "windows/amd64")
+platforms=("linux/amd64")
 
 for platform in "${platforms[@]}"
 do
 	platform_split=(${platform//\// })
 	GOOS=${platform_split[0]}
 	GOARCH=${platform_split[1]}
-	output_name=$package_name'-'$GOOS'-'$GOARCH
+	dir_name="$GOOS-$GOARCH"
+	build_dir="build/$dir_name"
+	output_name="$build_dir/$package_name"
 	if [ $GOOS = "windows" ]; then
 		output_name+='.exe'
 	fi
 
-	env GOOS=$GOOS GOARCH=$GOARCH go build -o build/$output_name $package
+	# providing static assets for the server
+	mkdir -p "$build_dir/assets"
+	cp -r assets/samples "$build_dir/assets"
+
+	env GOOS="$GOOS" GOARCH="$GOARCH" go build -o $output_name $package
 	if [ $? -ne 0 ]; then
    		echo 'An error has occurred! Aborting the script execution...'
 		exit 1
 	fi
+
+	# archive the builds
+	(
+    cd build
+    if [ $GOOS = "windows" ]; then
+      zip -r "$dir_name.zip" "$dir_name"
+    else
+      tar -czf "$dir_name.tar.gz" "$dir_name"
+    fi
+  )
+
+  # remove the build folder
+  rm -rf "$build_dir"
 done
