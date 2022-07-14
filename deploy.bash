3b33822b65961a1a578a38525f54ab29dddf77e8
Ihar Suvorau <ihar.suvorau@gmail.com>
2022-07-14 16:03:01 +0300

    Deploy script updated

--- deploy.bash
+++ deploy.bash
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+image_name="nokal/waiting-time-backend"
+container_name="waiting-time-backend"
 remote_host=193.40.11.233
 results_dir=/home/ihar/deployments/waiting-time-backend/results
 
-ssh $remote_host docker pull nokal/waiting-time-backend
-ssh $remote_host docker stop waiting-time-backend
-ssh $remote_host docker rm waiting-time-backend
-ssh $remote_host docker run -d -p 80:8080 -e WEBAPP_HOST=$remote_host -v $results_dir:/srv/webapp/assets/results --name waiting-time-backend nokal/waiting-time-backend
+ssh $remote_host docker pull $image_name
+ssh $remote_host docker stop $container_name
+ssh $remote_host docker rm $container_name
+ssh $remote_host docker run -d -p 80:8080 -e WEBAPP_HOST=$remote_host -v $results_dir:/srv/webapp/assets/results --name $container_name $image_name
